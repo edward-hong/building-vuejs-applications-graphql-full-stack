3e86bfb5450dc575bf8d93fe2365467325e09360
Edward Hong <edward.hong527@gmail.com>
2021-10-20 01:02:15 +1300

    C01L03 Creating the hello world component

---
+++ my-component/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app">
+    <current-time />
+  </div>
+</template>
+
+<script>
+  import CurrentTime from './components/CurrentTime.vue'
+
+  export default {
+    name: 'app',
+    components: {
+      CurrentTime,
+    },
+  }
+</script>
