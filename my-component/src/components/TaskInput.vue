d54f6c11f7d22eca2953fa2782c0db1300aa9d9e
Edward Hong <edward.hong527@gmail.com>
2021-10-20 21:48:57 +1300

    C01S07 Creating a dynamic to-do list

--- my-component/src/components/TaskInput.vue
+++ my-component/src/components/TaskInput.vue
@@ -13,33 +13,33 @@
 </template>
 
 <script>
-export default {
-  name: 'TaskInput',
-  data: () => ({
-    task: '',
-  }),
-  methods: {
-    addTask() {
-      this.$emit('add-task', this.task)
-      this.task = ''
+  export default {
+    name: 'TaskInput',
+    data: () => ({
+      task: '',
+    }),
+    methods: {
+      addTask() {
+        this.$emit('add-task', this.task)
+        this.task = ''
+      },
     },
-  },
-}
+  }
 </script>
 
 <style scoped>
-.tasker {
-  margin: 20px;
-}
-.tasker .taskInput {
-  font-size: 14px;
-  margin: 0 10px;
-  border: 0;
-  border-bottom: 1px solid rgba(0, 0, 0, 0, 75);
-}
-.tasker button {
-  border: 1px solid rgba(0, 0, 0, 0, 75);
-  border-radius: 3px;
-  box-shadow: 0 1px 2px 0 rgba(0, 0, 0, 0.2);
-}
+  .tasker {
+    margin: 20px;
+  }
+  .tasker .taskInput {
+    font-size: 14px;
+    margin: 0 10px;
+    border: 0;
+    border-bottom: 1px solid rgba(0, 0, 0, 0, 75);
+  }
+  .tasker button {
+    border: 1px solid rgba(0, 0, 0, 0, 75);
+    border-radius: 3px;
+    box-shadow: 0 1px 2px 0 rgba(0, 0, 0, 0.2);
+  }
 </style>
