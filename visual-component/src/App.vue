62e01c508c671e20b71ef737680e1ef8bf048aca
Edward Hong <edward.hong527@gmail.com>
2021-10-21 12:13:26 +1300

    C02S03 Using slots and named slots to place data inside your components

--- visual-component/src/App.vue
+++ visual-component/src/App.vue
@@ -1,6 +1,19 @@
 <template>
   <div id="app">
-    <material-card-box />
+    <MaterialCardBox>
+      <template v-slot:header>
+        <strong>Card Title</strong><br />
+        <span>Card Sub-Title</span>
+      </template>
+      <template v-slot:media>
+        <img src="https://via.placeholder.com/350x150" />
+      </template>
+      <p>Main Section</p>
+      <template v-slot:action>
+        <button>Action Button</button>
+        <button>Action Button</button>
+      </template>
+    </MaterialCardBox>
   </div>
 </template>
 
