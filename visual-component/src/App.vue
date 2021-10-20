236bcec2e2bd3686d4897982c10faa370b9f4cac
Edward Hong <edward.hong527@gmail.com>
2021-10-20 23:28:44 +1300

    C02S02 Creating a visual template component

---
+++ visual-component/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app">
+    <material-card-box />
+  </div>
+</template>
+
+<script>
+  import MaterialCardBox from './components/MaterialCardBox'
+
+  export default {
+    name: 'app',
+    components: {
+      MaterialCardBox,
+    },
+  }
+</script>
