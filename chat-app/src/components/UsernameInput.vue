555a66df139b63e9373143974c0743f6cb041a3f
Edward Hong <edward.hong527@gmail.com>
2021-11-11 23:51:36 +1300

    C04S01 Creating custom inputs for the application

---
+++ chat-app/src/components/UsernameInput.vue
@@ -0,0 +1,24 @@
+<template>
+  <q-input
+    :value="value"
+    :rules="[
+      (val) => (val && val.length > 5) || 'Please type a valid Username',
+    ]"
+    outlined
+    label="Your Username"
+    lazy-rules
+  />
+</template>
+
+<script>
+export default {
+  name: "UsernameInput",
+  props: {
+    value: {
+      type: String,
+      default: "",
+      required: false,
+    },
+  },
+};
+</script>
